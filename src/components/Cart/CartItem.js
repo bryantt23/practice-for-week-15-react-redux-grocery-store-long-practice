ef03af9c09b9c19406301cb572075d8812312653
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-12-12 23:21:31 -0800

    remove item

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,32 +1,28 @@
 import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { removeItem } from '../../store/cart';
 
 function CartItem({ item }) {
   const [count, setCount] = useState(item.count);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setCount(item.count);
   }, [item.count]);
 
   return (
-    <li className="cart-item">
-      <div className="cart-item-header">{item.name}</div>
-      <div className="cart-item-menu">
-        <input
-          type="number"
-          value={count}
-        />
+    <li className='cart-item'>
+      {JSON.stringify(item)}
+      <div className='cart-item-header'>{item.name}</div>
+      <div className='cart-item-menu'>
+        <input type='number' value={count} />
+        <button className='cart-item-button'>+</button>
+        <button className='cart-item-button'>-</button>
         <button
-          className="cart-item-button"
-        >
-          +
-        </button>
-        <button
-          className="cart-item-button"
-        >
-          -
-        </button>
-        <button
-          className="cart-item-button"
+          onClick={() => {
+            dispatch(removeItem(item.id));
+          }}
+          className='cart-item-button'
         >
           Remove
         </button>
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,4 +1,5 @@
 export const ADD_ITEM = 'cart/ADD_ITEM';
+export const REMOVE_ITEM = 'cart/REMOVE_ITEM';
 
 export const addItem = item => {
   return {
@@ -7,19 +8,27 @@ export const addItem = item => {
   };
 };
 
+export const removeItem = id => {
+  return {
+    type: REMOVE_ITEM,
+    id
+  };
+};
+
 export default function cartReducer(state = {}, action) {
   switch (action.type) {
     case ADD_ITEM:
       const stateCopy = { ...state };
       const { id, name } = action.item;
-
       if (!stateCopy[id]) {
-        stateCopy[id] = { count: 0, name };
+        stateCopy[id] = { count: 0, name, id };
       }
-
       stateCopy[id].count++;
-
       return stateCopy;
+    case REMOVE_ITEM:
+      const stateCopyWithRemoval = { ...state };
+      delete stateCopyWithRemoval[action.id];
+      return stateCopyWithRemoval;
     default:
       return state;
   }
